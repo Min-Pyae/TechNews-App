b22fd40f39e11153b9a80306a9fd4b209501f967
Min Pyae <minmp2002@gmail.com>
2024-01-02 14:23:53 +0000

    Updated User Interface in ContentView file and Created DetailView file

---
+++ TechNews App/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  TechNews App
+//
+//  Created by Chris Min on 02/01/2024.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
